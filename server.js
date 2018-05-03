2b1e67c2991be106548d49cdb459957f2441a674
nvidya.karthik <nvidya.karthik@gmail.com>
2018-05-03 11:58:16 -0700

    added basic code to connect to server,set view engine

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
